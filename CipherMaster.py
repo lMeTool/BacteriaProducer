cab5a28022004341b3b10a14bc14fa7b3b0bca07
Zenkin Danil <co0kisszda@gmail.com>
2025-06-25 10:52:32 +0800

    Solving the third task

--- CipherMaster.py
+++ CipherMaster.py
@@ -6,16 +6,26 @@ class CipherMaster:
         # Метод должен возвращать зашифрованный текст
         # с учетом переданного смещения shift.
         result = []
+        original_text = str.lower(original_text)
         for letter in original_text:
-            ...  # здесь ваш код
+            if self.alphabet.find(letter) != -1:
+                index = (self.alphabet.find(letter)+shift) % 33
+                result.append(self.alphabet[index])
+            else:
+                result.append(letter)
         return ''.join(result)
 
     def decipher(self, cipher_text, shift):
         # Метод должен возвращать исходный текст
         # с учётом переданного смещения shift.
+        cipher_text = str.lower(cipher_text)
         result = []
         for letter in cipher_text:
-            ...  # здесь ваш код
+            if self.alphabet.find(letter) != -1:
+                index = (self.alphabet.find(letter)-shift) % 33
+                result.append(self.alphabet[index])
+            else:
+                result.append(letter)
         return ''.join(result)
 
 
