4b2ef7eebc336c5b79e8a14d6e8e0a6983318580
Zenkin Danil <co0kisszda@gmail.com>
2025-06-25 09:39:41 +0800

    Created python file with third task

---
+++ CipherMaster.py
@@ -0,0 +1,30 @@
+class CipherMaster:
+    # Не изменяйте и не перемещайте эту переменную
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    def cipher(self, original_text, shift):
+        # Метод должен возвращать зашифрованный текст
+        # с учетом переданного смещения shift.
+        result = []
+        for letter in original_text:
+            ...  # здесь ваш код
+        return ''.join(result)
+
+    def decipher(self, cipher_text, shift):
+        # Метод должен возвращать исходный текст
+        # с учётом переданного смещения shift.
+        result = []
+        for letter in cipher_text:
+            ...  # здесь ваш код
+        return ''.join(result)
+
+
+cipher_master = CipherMaster()
+print(cipher_master.cipher(
+    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+    shift=2
+))
+print(cipher_master.decipher(
+    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+    shift=-3
+))
